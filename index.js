ebb44940e417eddf3c9d2c22ed722e6ad1adc8c7
Lawrence <lawrence.0725@hotmail.com>
2022-02-10 21:52:11 +0800

    Update JS touch

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+// Page loads
+function startup() {
+    var el = document.getElementById("infoButton");
+    el.addEventListener("touchstart", handleStart, false);
+    el.addEventListener("touchend", handleEnd, false);
+    el.addEventListener("touchcancel", handleCancel, false);
+    el.addEventListener("touchmove", handleMove, false);
+}
+
+// 
+function handleStart(evt) {
+    evt.preventDefault();
+    console.log("touchstart.");
+    var el = document.getElementById("infoCard");
+    el.style.visibility = 'visible';
+}
+
 // Chart
 const ctx = document.getElementById('pieChart').getContext('2d');
 const pieChart = new Chart(ctx, {
